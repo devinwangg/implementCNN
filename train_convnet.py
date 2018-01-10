5ff37bb09678d052d8a98156a13235e5e1947918
WeiXiang <once9125@gmail.com>
2018-01-11 02:22:48 +0800

    first

---
+++ train_convnet.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from common.trainer import Trainer
+from simple_convnet import SimpleConvNet
+from dataset.mnist import load_mnist
+
+
+# 讀取數據
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+
+max_epochs = 20
+
+network = SimpleConvNet(input_dim=(1,28,28), 
+                        conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+                        hidden_size=100, output_size=10, weight_init_std=0.01)
+                        
+trainer = Trainer(network, x_train, t_train, x_test, t_test,
+                  epochs=max_epochs, mini_batch_size=100,
+                  optimizer='Adam', optimizer_param={'lr': 0.001},
+                  evaluate_sample_num_per_epoch=1000)
+trainer.train()
+
+# 保存參數
+network.save_params("params.pkl")
+print("Saved Network Parameters!")
+
+# 繪製一個圖表
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(max_epochs)
+plt.plot(x, trainer.train_acc_list, marker='o', label='train', markevery=2)
+plt.plot(x, trainer.test_acc_list, marker='s', label='test', markevery=2)
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
